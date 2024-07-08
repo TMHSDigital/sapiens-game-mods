2401488bc847edbd6d409e1d109a2988a107c894
T <154358121+TMHSDigital@users.noreply.github.com>
2024-07-08 02:26:49 -0400

    Create modInfo.lua

---
+++ mods/super-speed-mod/modInfo.lua
@@ -0,0 +1,8 @@
+local modInfo = {
+    name = "Super Speed Mod",
+    description = "Makes Sapiens move 5x faster",
+    version = "1.0.0",
+    type = "world",
+    developer = "Your Name"
+}
+return modInfo
