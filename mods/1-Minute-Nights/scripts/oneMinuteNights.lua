747f2278b960ab70ac85caa71e67ed73b273703e
T <154358121+TMHSDigital@users.noreply.github.com>
2024-07-08 02:17:08 -0400

    Update oneMinuteNights.lua

--- mods/1-Minute-Nights/scripts/oneMinuteNights.lua
+++ mods/1-Minute-Nights/scripts/oneMinuteNights.lua
@@ -1,1 +1,29 @@
+local mod = {
+    loadOrder = 1
+}
+
+function mod:onload(world)
+    local nightStart = 0.5  -- Start of night (in game time)
+    local nightEnd = 0.75   -- End of night (in game time)
+    local realTimeDuration = 60  -- 1 minute in seconds
+    local gameSecondsInADay = 86400  -- Total game seconds in a day (24 hours * 60 minutes * 60 seconds)
+
+    local function adjustNightDuration(time)
+        if time >= nightStart and time <= nightEnd then
+            local nightDuration = (nightEnd - nightStart) * gameSecondsInADay
+            local realTimeRatio = realTimeDuration / nightDuration
+            world.timeScale = realTimeRatio  -- Adjust the time scale to match real time duration
+        else
+            world.timeScale = 1  -- Normal time scale during the day
+        end
+    end
+
+    local originalSetTime = world.setTime
+    world.setTime = function(time)
+        adjustNightDuration(time)
+        originalSetTime(time)
+    end
+end
+
+return mod
 
