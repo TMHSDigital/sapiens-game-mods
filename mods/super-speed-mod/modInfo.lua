bb3e80807a7789ce5348908a66c247efcd1dacd1
T <154358121+TMHSDigital@users.noreply.github.com>
2024-07-08 02:38:20 -0400

    Update modInfo.lua

--- mods/super-speed-mod/modInfo.lua
+++ mods/super-speed-mod/modInfo.lua
@@ -1,6 +1,6 @@
 local modInfo = {
     name = "Super Speed Mod",
-    description = "Makes Sapiens move 5x faster",
+    description = "Makes Sapiens in your tribe move 5x faster",
     version = "1.0.0",
     type = "world",
     developer = "Your Name"
