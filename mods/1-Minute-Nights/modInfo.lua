f80538a6ad987f4ee1ba20f10b89394028c2271a
T <154358121+TMHSDigital@users.noreply.github.com>
2024-07-08 02:16:23 -0400

    Create modInfo.lua

---
+++ mods/1-Minute-Nights/modInfo.lua
@@ -0,0 +1,8 @@
+local modInfo = {
+    name = "1 Minute Nights Mod",
+    description = "Sets the duration of night time to 1 real-time minute",
+    version = "1.0.0",
+    type = "world",
+    developer = "Your Name"
+}
+return modInfo
